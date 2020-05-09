de0c1fb866e25461ecbca7943b23deaf043eb1c2
Kaskeeeee <64480145+Kaskeeeee@users.noreply.github.com>
2020-05-09 16:21:26 +0200

    Update server.c

--- system_calls/codice/server.c
+++ system_calls/codice/server.c
@@ -12,9 +12,10 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/sem.h>
+#include <sys/msg.h>
 #include <unistd.h>
 
-#define ACK_LIST_SEM 0;
+//#define ACK_LIST_SEM 0;
 
 
 /*Declaration of methods*/
@@ -22,23 +23,23 @@ void sigTermHandlerServer(int sig);
 void updatePosition(int pos_file_fd, int device_no);
 void print_positions(int step);
 void sigTermHandlerDevice(int sig);
+void initialize(Message * dev_msg_list, int size);
 
 /*Global variables*/
-int shmidBoard;
-struct Board {
-    int matrix[BOARD_ROWS][BOARD_COLS];
-} * board;
+int shmidBoard;                         //shared memory board id
+struct Board * board;                   //board pointer
 
-int shmidAckList;
-Acknowledgment * ackList;
-int board_semid;
-int acklist_semid
-int devices_positions[N_DEVICES][2];
-pid_t children_pids[1 + N_DEVICES];
+int shmidAckList;                       //acklist id
+Acknowledgment * ackList;               //acklist pointer
+int sem_idx_board;                      //semaphore board id
+int sem_idx_ack;                        //semaphore acklist id
+int sem_idx_access;
+pid_t children_pids[1 + N_DEVICES];     //array used to keep all the pids of generated processes
+int prevX = -1, prevY = -1;             //variables to keep track of last position in order
+                                        //to clear it when the devices moves away
 
-int serverFIFO;
-int serverFIFO_extra;
-char pathToMyFIFO[25];
+int serverFIFO;                         //serverFIFO id
+char pathToMyFIFO[25];                  //pathname to fifo
 
 /*Main*/
 int main(int argc, char * argv[]) {
@@ -70,7 +71,7 @@ int main(int argc, char * argv[]) {
     if(signal(SIGTERM, sigTermHandlerServer) == SIG_ERR)
         errExit("signal failed");
 
-    printf("<Server> Server ready!\n");
+    printf("<Server> Server ready! [PID: %d]\n", getpid());
 
     //Checking if the file exists and opening it
     printf("<Server> Opening the file: %s...\n", argv[2]);
@@ -94,20 +95,27 @@ int main(int argc, char * argv[]) {
 
     //Setting semaphores
     //Board semaphore set
-    board_semid = semget(IPC_PRIVATE, N_DEVICES, IPC_CREAT | S_IRUSR | S_IWUSR);
+    sem_idx_board = semget(IPC_PRIVATE, N_DEVICES, IPC_CREAT | S_IRUSR | S_IWUSR);
 
     unsigned short initialValues[] = {0, 0, 0, 0, 0};
     union semun arg;
     arg.array = initialValues;
 
-    if(semctl(board_semid, 0, SETALL, arg) == -1)
+    if(semctl(sem_idx_board, 0, SETALL, arg) == -1)
         errExit("semctl failed");
 
     //Acknowledge list semaphore set
-    acklist_semid = semget(IPC_PRIVATE, 1, IPC_CREAT | S_IRUSR | S_IWUSR);
-    if(semctl(acklist_semid, 0, SETVAL, 0) == -1)
+    sem_idx_ack = semget(IPC_PRIVATE, 1, IPC_CREAT | S_IRUSR | S_IWUSR);
+    arg.val = 0;
+    if(semctl(sem_idx_ack, 0, SETVAL, arg) == -1)
         errExit("semctl failed");
     
+    //Board access semaphore
+    sem_idx_access = semget(IPC_PRIVATE, 1, IPC_CREAT | S_IRUSR | S_IWUSR);
+    arg.val = N_DEVICES;
+    if(semctl(sem_idx_access, 0, SETVAL, arg) == -1)
+        errExit("semctl failed");
+
     //Generating children processes
     children_pids[0] = fork();
 
@@ -117,6 +125,56 @@ int main(int argc, char * argv[]) {
         /*-----------------------------
                 ACK_MANAGER CODE
          -----------------------------*/
+        
+        //Creating a message queue
+        int msqid = msgget(msgKey, IPC_CREAT | S_IRUSR | S_IWUSR);
+        if(msqid == -1)
+            errExit("msgget failed");
+        
+        //the list keeps divided the different
+        //message ids
+        
+        while(1){
+            semOp(sem_idx_ack, 0, -1);
+            //for loop to check if there are 5 acks of a message id
+            //and to free memory
+            for(int i = 0; i < ACK_LIST_SIZE; i++){
+                int count = 1;
+                //save the message id
+                int m_id = ackList[i].message_id;
+                //check if it's a real message id
+                if(m_id != 0){
+                    //check if there are N_DEVICES (5) acknowledgements
+                    for(int j = i + 1; j < i + N_DEVICES; j++){
+                        if(ackList[j].message_id == m_id)
+                            count++;
+                    }
+                    if(count == N_DEVICES){
+                        //create the message for the message queue;
+                        struct ackMessage message_to_client;
+                        //mtype will be the pid of the client since
+                        //ackList[i] identify the first ack that has
+                        //client's pid in the field pid_sender
+                        message_to_client.mtype = ackList[i].pid_sender;
+                        int j;
+                        for(j = i; j < i + N_DEVICES; j++){
+                            message_to_client.acks[j] = ackList[j];
+                            //mark the position as free
+                            ackList[j].message_id = 0;
+                        }
+                        //tell the last device to delete last message
+                 //TODO       kill(ackList[j-1].pid_receiver, SIGUSR1);
+                        //send message
+                        if(msgsnd(msqid, &message_to_client, sizeof(struct ackMessage), 0) == -1)
+                            errExit("msgsnd failed");
+                    }
+                }
+                
+            }
+            semOp(sem_idx_ack, 0, 1);
+            sleep(5);
+        }
+
     }
 
     for(int i = 0; i < N_DEVICES; i++){
@@ -135,20 +193,33 @@ int main(int argc, char * argv[]) {
             if(mkfifo(pathToMyFIFO, S_IRUSR | S_IWUSR) == -1)
                 errExit("fifo not created");
 
-            serverFIFO = open(pathToMyFIFO, O_RDONLY);
-            serverFIFO_extra = open(pathToMyFIFO, O_WRONLY);
+            //Opening FIFO in non blocking mode
+            serverFIFO = open(pathToMyFIFO, O_RDONLY | O_NONBLOCK);
+            if(serverFIFO == -1)
+                errExit("fifo open failed");
 
             int device_no = i;
 
-            semOp(board_semid, device_no, -1);
+            semOp(sem_idx_board, device_no, -1);
             updatePosition(fd, device_no);
             if(device_no != N_DEVICES - 1)
-                semOp(board_semid, device_no + 1, 1);
+                semOp(sem_idx_board, device_no + 1, 1);
 
-            //Message msgsList[10] = {NULL};
+            //when all devices join the board
+            //this semaphore will unlock the server
+            //so it can print positions
+            semOp(sem_idx_access, 0, -1);
 
+            //List of device messages
+            Message msgList[20];
+            //init the list
+            initialize(msgList, 20);
             while(1){
-                
+                //Invio messaggi
+                send_messages(board, prevX, prevY, msgList, 20);
+
+                //Get the messages and update acknowledgement list
+                receive_update(msgList, 20, ackList, sem_idx_ack, serverFIFO);
             }
 
         }
@@ -157,12 +228,22 @@ int main(int argc, char * argv[]) {
 
     /*-----------------------------
              SERVER CODE
-       ---------------------------*/ 
+       ---------------------------*/
+        
     int step = 0;
     while(1){
-        semOp(board_semid, 0, 1);
-        sleep(2);
+        //Unlocks the first device
+        semOp(sem_idx_board, 0, 1);
+        
+        //Waits for all the devices to updatePosition()
+        //and prints the positions
+        semOp(sem_idx_access, 0, 0);
         print_positions(step);
+
+        //Set again the semaphore to N_DEVICES
+        semOp(sem_idx_access, 0, N_DEVICES);
+
+        sleep(2);
         step++;
     } 
 }
@@ -183,9 +264,6 @@ void sigTermHandlerDevice(int sig){
     if (serverFIFO != 0 && close(serverFIFO) == -1)
         errExit("close failed");
 
-    if (serverFIFO_extra != 0 && close(serverFIFO_extra) == -1)
-        errExit("close failed");
-
     // Remove the FIFO
     if (unlink(pathToMyFIFO) != 0)
         errExit("unlink failed");
@@ -197,33 +275,55 @@ void sigTermHandlerDevice(int sig){
 //position of a device and updates it in the board matrix
 //and in the devices_positions matrix
 void updatePosition(int pos_file_fd, int device_no){
+    //read the first 3 characters of the file
     char buffer[4];
     int bR = read(pos_file_fd, buffer, 3);
     
+    //buffer now should contain a string
+    //with the format: "x,y"
+
     if(bR != -1){
         buffer[bR] = '\0';
+        //lseek skips the next '|' or '\n'
         lseek(pos_file_fd, 1, SEEK_CUR);
     }
     else errExit("read failed");
 
-    int x = (int)(buffer[0] - '0');
-    int y = (int)(buffer[2] - '0');
+    //if you haven't reached EOF
+    //then update position
+    if(bR != 0){
+        int x = (int)(buffer[0] - '0');
+        int y = (int)(buffer[2] - '0');
 
-    if(board->matrix[x][y] != 0){
-        printf("Error, cell is not free!\n")
-        kill(getppid(), SIGTERM);
+        if(board->matrix[x][y] != 0){
+            printf("Error, cell is not free!\n");
+            kill(getppid(), SIGTERM);
+        }
+
+        if(prevX != -1 && prevY != -1)
+            board->matrix[prevX][prevY] = 0;
+        board->matrix[x][y] = getpid();
+        prevX = x;
+        prevY = y;
     }
-    
-    board->matrix[x][y] = getpid();
-    devices_positions[device_no][0] = x;
-    devices_positions[device_no][1] = y;
 }
 
 //the print_positions method is used to 
 //print informations about devices' positions
 void print_positions(int step){
-    printf("# Step %d: device positions #############", step)
-    for(int i = 0; i < N_DEVICES; i++)
-        printf("%d  %d  %d  msgs: %s", children_pids[i+1], devices_positions[i][0], devices_positions[i][1], /*lista*/);
+    int x, y;
+    printf("# Step %d: device positions #############", step);
+    for(int i = 0; i < N_DEVICES; i++){
+        pid_t devpid = children_pids[i+1];
+        getPosition(devpid, board, &x, &y);
+        printf("%d  %d  %d  msgs: ", devpid, x, y);
+        print_device_msgs(devpid);
+        printf("\n");
+    }
     printf("#########################################");
+}
+
+void initialize(Message * dev_msg_list, int size){
+    for(int i = 0; i < size; i++)
+        dev_msg_list[i].message_id = -1;
 }
